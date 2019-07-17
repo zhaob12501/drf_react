2d801b0f43044bc05bb015e8e638f4732f8e3cec
赵彬 <zhao12501@163.com>
2019-07-17 15:58:46 +0800

    生成测试报告

--- drf_react/settings.py
+++ drf_react/settings.py
@@ -105,15 +105,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 数据库存储使用时间，True时间会被存为UTC的时间
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
